cae7a53227e247c8b969bb023dec4424c97214d5
Pruthviraj Bhosale <bpruthviraj248@gmail.com>
2026-06-05 22:56:52 +0530

    First Commit

---
+++ variable.js
@@ -0,0 +1,38 @@
+// redeclaration - not possible
+// let a="hello"
+// let a=20;
+// console.log(a)
+
+
+// // reinitialization -  possible
+// let a = "hello"
+// a = 20;
+// console.log(a)
+
+
+// hoisting - not possible
+// a = 10;
+// let a;
+// console.log(a)
+
+
+// redeclaration - not possible
+// const a = "hello"
+// const a = 20;
+// console.log(a)
+
+
+//reinitialization - not possible
+// const a = "hello"
+// a = 20;
+// console.log(a)
+
+
+// hoisting - not possible
+// a=10;
+// const a;
+// console.log(a)
+
+
+
+
