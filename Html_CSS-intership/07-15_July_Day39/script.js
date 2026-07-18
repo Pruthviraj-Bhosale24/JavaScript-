587f505462e343c1984f12adc66b82876d7c4887
Pruthviraj Bhosale <bpruthviraj248@gmail.com>
2026-07-18 21:03:18 +0530

    Remove Groq API key

--- Html_CSS-intership/07-15_July_Day39/script.js
+++ Html_CSS-intership/07-15_July_Day39/script.js
@@ -2,7 +2,7 @@ const input = document.getElementById("user-input");
 const btn = document.getElementById("sent-btn");
 const chatBox = document.getElementById("chat-box");
 
-const apiKey = "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";
+const apiKey = "mi_api_key";
 
 btn.addEventListener("click", sendMessage);
 
