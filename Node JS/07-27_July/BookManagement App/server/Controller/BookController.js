b8b1e1d8a198fe68c2638c3d898b3becdf3503d7
Pruthviraj Bhosale <bpruthviraj248@gmail.com>
2026-07-31 23:54:31 +0530

    Code updated

--- Node JS/07-27_July/BookManagement App/server/Controller/BookController.js
+++ Node JS/07-27_July/BookManagement App/server/Controller/BookController.js
@@ -1,3 +1,4 @@
+const book = require("../Model/book");
 const Book = require("../Model/book");
 
 const handleAddBookController = async (req, res) => {
@@ -9,4 +10,29 @@ const handleAddBookController = async (req, res) => {
     }
 };
 
-module.exports = { handleAddBookController };
+
+//display ALL book
+const handlegetAllBookController= async(req,res)=>{
+    try{
+        const booklist = await book.find({})
+        return res.status(200).json({message:"Book Details fetched", booklist})
+    }catch(err){
+        return res.status(500).json({message:err.message})
+    }
+}
+
+
+
+//delete books
+const  handleDeleteBookController = async(req,res)=>{
+    try{
+        const data = req.body
+        const deleted= await book.deleteOne({_id:data._id})
+        console.log("Book deleted");
+        
+        return res.status(200).json({message:"Book Deleted..."})
+    }catch(err){
+          return res.status(500).json({message:err.message})
+    }
+}
+module.exports = { handleAddBookController, handlegetAllBookController , handleDeleteBookController };
--- Node JS/07-27_July/BookManagement App/server/router/bookroute.js
+++ Node JS/07-27_July/BookManagement App/server/router/bookroute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { handleAddBookController } = require("../Controller/BookController");
+const { handleAddBookController, handlegetAllBookController, handleDeleteBookController } = require("../Controller/BookController");
 
 const router = express.Router();
 
@@ -7,6 +7,10 @@ router.get("/msg", (req, res) => {
     res.send("Respond from book route");
 });
 
-router.post("/", handleAddBookController);
+router.post("/addBook", handleAddBookController);
+router.get("/getBook", handlegetAllBookController)
+router.delete("/deleteBook", handleDeleteBookController)
+
+
 
 module.exports = router;
