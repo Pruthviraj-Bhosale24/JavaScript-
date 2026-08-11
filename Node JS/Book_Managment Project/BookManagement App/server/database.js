b203e2b336de49347dc6cf87d3205d04eceea638
Pruthviraj Bhosale <bpruthviraj248@gmail.com>
2026-08-11 20:58:57 +0530

    code updated

--- Node JS/Book_Managment Project/BookManagement App/server/database.js
+++ Node JS/Book_Managment Project/BookManagement App/server/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = () => {
     mongoose.connect("mongodb://bpruthviraj248_db_user:36vqH3tofX0Nf28G@ac-1mtxvpm-shard-00-00.nspitlw.mongodb.net:27017,ac-1mtxvpm-shard-00-01.nspitlw.mongodb.net:27017,ac-1mtxvpm-shard-00-02.nspitlw.mongodb.net:27017/?ssl=true&replicaSet=atlas-lbkqen-shard-0&authSource=admin&appName=bookmanagment")
         .then(() => {
-            console.log("DB connected successfully.....");
+            console.log("DB connected successfully...");
         })
         .catch((err) => {
             console.log(err);
